503f289f842e2bede6853bc32d118f2902afa29f
Suraj Singh <yousurajsingh@gmail.com>
2025-01-29 00:21:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function getFormvalue() {
     //Write your code here
 	const form = document.getElementById("form1");
-	const first= form.elements["first_name"].value;
-	const last=form.elements["last_name"].value;
+	const first= form.elements["fname"].value;
+	const last=form.elements["lname"].value;
 	alert(`${first} ${last}`);
 
 }
